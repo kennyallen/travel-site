16935732145269d61824b004e6d1fb924bfcae58
Kenny <kennydavidallen@gmail.com>
2018-03-01 23:19:01 +0800

    Added Gulp with some PostCSS packages.

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
+
+gulp.task('default', function(){
+	console.log("Hooray! You created a Gulp task, kiddo.");
+});
+
+gulp.task('html', function(){
+	console.log("Imagine something useful happening with your HTML here.");
+});
+
+gulp.task('styles', function(){
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([nested, cssvars, autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles'));
+});
+
+
+
+
+gulp.task('watch', function(){
+	
+	watch('./app/index.html', function(){
+		gulp.start('html');
+	});
+
+	watch('./app/assets/styles/**/*.css', function(){
+		gulp.start('styles');
+	});
+
+});
